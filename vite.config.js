e558cb82d78218e604dbd440e829b003107c83b1
VarunBhardwaj1 <varuncv78@gmail.com>
2025-07-21 20:15:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/BillSplit.github.io/'
+  base: '/BillSplit.github.io/',
+  plugins: [react()],
 })
